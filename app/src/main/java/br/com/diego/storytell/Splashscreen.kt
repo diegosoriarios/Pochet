5d5e16b888b31cc2551b523abe9fb2d2565cd82d
diegosoriarios <diegosoriarios@gmail.com>
2020-06-26 18:55:13 -0300

    Splashscreen

---
+++ app/src/main/java/br/com/diego/storytell/Splashscreen.kt
@@ -0,0 +1,22 @@
+package br.com.diego.storytell
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+        val handle: Handler = Handler()
+        handle.postDelayed({ nextPage() }, 2000)
+    }
+
+    private fun nextPage() {
+        val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
